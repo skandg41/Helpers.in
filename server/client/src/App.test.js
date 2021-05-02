eb28e8c81b1c78bdebc5fb2ce4c8cf3ae8756370
Skand <skandg41@gmail.com>
2021-05-02 20:44:52 +0530

    Frontend testing added

--- server/client/src/App.test.js
+++ server/client/src/App.test.js
@@ -1,9 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { getByText, render, fireEvent, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import App from './App';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import React from 'react'
+import {Provider} from 'react-redux';
+import store from './store';
+
+
+describe('Testing', function() {
+    const { queryByTitle, getByText } = render(<App />);
+    describe("<App />", () => {
+      it(" Check whether app renders or not?", () => {
+        //const { getByText } = render(<App />);
+        expect(getByText(/Hire/i)).toBeInTheDocument();
+      });
+    });   
+
+    it('Login page loading or not', function () {
+      const { queryByTitle, getByText } = render(<Provider store={store}>
+              <App />
+            </Provider>);
+            //Case-1
+            fireEvent.click(getByText("Log In"));
+            expect(getByText(/account/i)).toBeInTheDocument();
+            });
+          
+         
+  
+
+      it('Register page loading or not', function () {
+          const { queryByTitle, getByText } = render(<Provider store={store}>
+              <App />
+            </Provider>);
+                //Case-1
+                fireEvent.click(getByText("Register"));
+                expect(getByText(/Register/i)).toBeInTheDocument();
+                });
+              
+          
+    
+return
 });
