1362b525741eaed64d9e039a4eab521988c543c0
Skand Gupta <skandg41@gmail.com>
2021-05-12 21:08:57 +0530

    .

--- server/twilio/index.js
+++ server/twilio/index.js
@@ -1,13 +1,13 @@
 const logger = require('../logger');
 function sendmsg(number,msg){
 
-  const accountSid = 'AC1a6757b28d8774d8318faa99b283a818'; 
-  const authToken = '49fca941ec666b28a07e26866b2730d3'; 
+  const accountSid = 'twilio account sid here'; 
+  const authToken = 'twilio auth token here'; 
   const client = require('twilio')(accountSid, authToken); 
   client.messages 
         .create({ 
            body: msg,  
-           messagingServiceSid: 'MG2a7083937a868884d10d77d2740335d7',      
+           messagingServiceSid: 'twilio message service sid here',      
            to: '+91'+number 
          }) 
         .then(message => logger.info("SMS "+ msg +" send to "+number + " sid "+message.sid))
